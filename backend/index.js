70aa9882cdde5cbd31fa46d4d02438db2ec53ae3
Amol Dike <amoldike002@gmail.com>
2023-02-27 12:45:46 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const conn = require("./db");
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 
 app.use(express.json());
 app.use(cors());
